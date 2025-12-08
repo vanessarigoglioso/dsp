0ff2dfdcbd7c28445315a2d65fd9f963539ad384
Vanessa Rigoglioso <74262359+vanessarigoglioso@users.noreply.github.com>
2025-12-08 11:11:41 -0500

    Enhance dspfinal.m to include ASCII and binary conversion
    Added conversion of message to ASCII and binary.

--- dspfinal.m
+++ dspfinal.m
@@ -4,4 +4,7 @@ close all
 
 message = input("Input Text Message:\n", "s");    % Input Message from User
 fprintf(message + "\n")
+asciiValues = uint8(message);
+binMessage=dec2bin(asciiValues, 8);
+
 
