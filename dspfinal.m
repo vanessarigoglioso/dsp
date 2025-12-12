ed350d19b55f4b4243830b8e87b984be0d116c98
03Vacations20 <pcervantes114@gmail.com>
2025-12-12 16:53:17 -0500

    Refactor dspfinal.m with comments and structure
    Reorganized code for clarity and added comments for better understanding.

--- dspfinal.m
+++ dspfinal.m
@@ -1,12 +1,18 @@
-clc
 clear
 close all
+clc
 
-message = input("Input Text Message:\n", "s");    % Input Message from User
+% User Message Input:
+message = input("Input Text Message:\n", "s");
 fprintf(message + "\n")
-asciiValues = uint8(message);
-binMessage=dec2bin(asciiValues, 8);
 
+% Convert text to binary
+asciiValues = uint8(message); % Characters mapped to ASCII Integer Values
+binMessage=dec2bin(asciiValues, 8) % ASCII Integer Values mapped to 8-bit binary
+% msg_length = numel(asciiValues);
+
+width = 100;
 for i = 1:length(binMessage)
 end
+
 
