c374944906dad0576ab82e4ef45c951bafc18e31
Vanessa Rigoglioso <74262359+vanessarigoglioso@users.noreply.github.com>
2025-12-12 16:57:38 -0500

    Refactor dspfinal.m to improve readability

--- dspfinal.m
+++ dspfinal.m
@@ -14,8 +14,9 @@ binMessage=dec2bin(asciiValues, 8) % ASCII Integer Values mapped to 8-bit binary
 width = 100;
 bit_text_msg = reshape(binMessage.', 1, [])
 
-for i = 1:length(binMessage)
+for i = 1:length(bit_text_msg)
 end
+
 
 
 
